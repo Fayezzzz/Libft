252d2e64b43d77ffce7b57abc51e0b83de070ca9
Muhammad faiez Khairul muhsein <mkhairul@u81z01s06.42kl.edu.my>
2022-06-10 22:30:45 +0800

    finished

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,17 +17,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*new_elem;
 
-	if (!f || !lst)
+	if (!lst)
 		return (NULL);
-	new_lst = NULL;
+	new_elem = ft_lstnew(f(lst->content));
+	if (!new_elem)
+		return (NULL);
+	new_lst = new_elem;
+	lst = lst->next;
 	while (lst)
 	{
-		if (!(new_elem = ft_lstnew(f(lst->content))))
+		new_elem->next = ft_lstnew(f(lst->content));
+		if (!(new_elem->next))
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_lst, new_elem);
+		new_elem = new_elem->next;
 		lst = lst->next;
 	}
 	return (new_lst);
